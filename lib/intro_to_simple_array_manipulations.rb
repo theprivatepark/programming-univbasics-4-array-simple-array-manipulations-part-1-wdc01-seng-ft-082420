e200a2a8780299d67926f50950def37fb32a659b
Anney Park <anneypark@gmail.com>
2020-07-20 21:34:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,6 @@
-def using_push (colors_in_the_rainbow, next_color)
-  updated_array = (colors_in_the_rainbow.push(next_color)
-  using_push()
+def using_push (array, string)
+  array.push(string)
   
 end
+
+
