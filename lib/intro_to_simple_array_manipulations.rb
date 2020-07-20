eb564eb84c60a1dc619a98bbac62003b64848106
Anney Park <anneypark@gmail.com>
2020-07-20 21:21:42 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,11 @@
+def using_push(array,string)
+  array.push(string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def deleted_string(array, element)
+  array.pop() = element
+end
